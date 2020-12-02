4a40444bf3ebc4b1b60b2aa9fa00693b28e02d28
Taeryn0112 <43917391+Taeryn0112@users.noreply.github.com>
2020-12-02 18:23:45 -0500

    Initial commit

---
+++ TwoNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoNotes
+//
+//  Created by Terry Lee on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
