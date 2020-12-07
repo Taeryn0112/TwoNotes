53732f9d5d4a43ebee9a282acebd9fa7f63721bf
Taeryn0112 <43917391+Taeryn0112@users.noreply.github.com>
2020-12-07 18:33:49 -0500

    Gesture and TableView Update
    Dismiss keyboard when "Done" gets tapped.  Texts in NotesViewController gets passed to NotesTableViewController's cell when dismissed.

--- TwoNotes/Controller/NotesViewController.swift
+++ TwoNotes/Controller/NotesViewController.swift
@@ -7,20 +7,27 @@
 
 import UIKit
 
-public class NotesViewController: UIViewController {
+public class NotesViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var noteTextView: UITextView!
     
         var note: Note!
         
-    @IBAction func done(_ sender: Any) {
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
         note.userInput = noteTextView.text
-        dismiss(animated: true, completion: nil)
+        
     }
     
-    @IBAction func unwindToRootViewController(segue: UIStoryboardSegue) {
-        print("Unwind to root view controller")
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneTapped))
     }
     
-    
+    @objc public func doneTapped() {
+        print("done")
+        noteTextView.resignFirstResponder()
+    }
     
 }
